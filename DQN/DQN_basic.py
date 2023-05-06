62180540c787ba47579005f22e8c6d26dd82a3bb
Bob Liu <527080292@qq.com>
2023-05-06 22:00:02 +0800

    finish the basic structure of the DQN

--- DQN/DQN_basic.py
+++ DQN/DQN_basic.py
@@ -37,3 +37,53 @@ class QNetwork(tf.keras.Model):
 if __name__=='__main__':
     env=gym.make('CartPole-v1')
     model=QNetwork
+    optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    replay_buffer=deque(maxlen=10000) #使用一个deque作为Qlearning的经验回放池
+    epsilon=initial_epsilon
+    for episode_id in range(num_episodes):
+        state=env.reset()#初始化环境，获得初始状态
+        epsilon=max(#计算当前探索率
+            initial_epsilon*(num_exploration_episode-episode_id)/num_exploration_episode,final_epsilon
+        )
+        for t in range(max_len_episode):
+            env.render()  #对当前帧进行渲染，绘图到屏幕
+            #epsilon-greedy探索策略，以epsilon的概率选择随机动作
+            if random.random()<epsilon:
+                action=env.action_space.sample()  #选择随机动作（探索）
+            else:
+                #选择模型计算出的Qvalue最大的动作
+                action=model.predict(np.expand_dims(state,axis=0)).numpy()
+                action=action[0]
+
+            #让环境执行动作，获得执行完动作的下一个状态、动作的奖励，游戏是否已结束以及额外信息
+            next_state,reward,done,info=env.step(action)
+            #如果游戏结束，给予大的负奖励
+            reward=-10. if done else reward
+            #将（state,action,reward,next_state)的四元组（外加done标签表示是否结束）放入经验回放池
+            replay_buffer.append((state,action,reward,next_state,1 if done else 0))
+            #更新当前状态
+            state=next_state
+
+            if done:#如果游戏结束，则退出本轮循环，进行下一个episode
+                print("episode %d, epsilon %f, score %d" %(episode_id,epsilon,t))
+                break
+            if len(replay_buffer)>=batch_size:
+                #从经验回放池中随机取一个批次的四元组，并分别转换为numpy数组
+                batch_state,batch_action,batch_reward,batch_next_state,batch_done=zip(*random.sample(replay_buffer,batch_size))
+                batch_state,batch_reward,batch_next_state,batch_done=\
+                    [np.array(a,dtype=np.float32) for a in [batch_state,batch_reward,batch_next_state,batch_done]]
+                batch_action=np.array(batch_action,dtype=np.int32)
+
+                q_value=model(batch_next_state)
+                y=batch_reward+(gamma*tf.reduce_max(q_value,axis=1))*(1-batch_done)#计算y值
+                with tf.GradientTape() as tape:
+                    loss=tf.keras.losses.mean_squared_error(
+                        #最小化y和Q value的距离
+                        y_true=y,
+                        y_pred=tf.reduce_sum(model(batch_state)*tf.one_hot(batch_action,
+                                                                           depth=2),axis=1)
+
+                    )
+                    grads=tape.gradient(loss,model.variables)
+                    #计算梯度 并更新参数
+                    optimizer.apply_gradients(grads_and_vars=zip(grads,model.variables))
